ebda9d5c516290d1c43e416aac57a90e3acc6423
“GrzechPL <grzech78@o2.pl>
2017-12-04 23:06:21 +0100

    04/12/17

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class TrelloController {
 
     @Autowired
+
     private TrelloClient trelloClient;
 
     @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
