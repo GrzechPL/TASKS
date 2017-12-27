b4ddd39f45ed1df5872c6bbf1a7ae7978915bb05
“GrzechPL <grzech78@o2.pl>
2017-12-27 23:32:00 +0100

    27/12/2017

--- src/test/java/com/crud/tasks/controller/TaskControllerTest.java
+++ src/test/java/com/crud/tasks/controller/TaskControllerTest.java
@@ -20,6 +20,7 @@ import java.util.Optional;
 
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -125,7 +126,29 @@ public class TaskControllerTest {
         //When & Then
         mockMvc.perform(delete("/v1/task/deleteTask?taskId=1"))
                 .andExpect(status().is(200));
+    }
+    @Test
+    public void updateTask() throws Exception {
+        //Given
+        Task task =new Task(
+                1L,
+                "Task",
+                "Content of Task");
 
+        TaskDto taskDto =new TaskDto(
+                1L,
+                "TaskDto",
+                "Content of TaskDto");
+
+        when(taskMapper.maptotaskDto(ArgumentMatchers.any(Task.class))).thenReturn(taskDto);
+        when(service.saveTask(ArgumentMatchers.any(Task.class))).thenReturn(task);
+
+        //When & Then
+        mockMvc.perform(put("/v1/task/updateTasks").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is(200))
+                .andExpect(jsonPath("$.id",is(1)))
+                .andExpect(jsonPath("$.title",is("Task")))
+                .andExpect(jsonPath("$.content",is("Content of Task")));
     }
 
 
