fb0ab0ff01de1f472738d845a0d053527bf54396
“GrzechPL <grzech78@o2.pl>
2018-01-17 11:36:56 +0100

    17/01/2018

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -10,4 +10,12 @@ public class AdminConfig {
 
     @Value("${admin.mail}")
     private String adminMail;
+
+    @Value("${admin.name}")
+    private String adminName;
+
+    @Value("${info.company.name}")
+    private String companyName;
+
+
 }
